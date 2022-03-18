974829da2aedecb20151eae0d7ad8dbcd42fb080
Parki <parki06780678@gmail.com>
2022-03-18 16:24:35 +0300

    test

--- task3/task3.py
+++ task3/task3.py
@@ -1,8 +1,7 @@
 import json
 import sys
 paths = sys.argv[1:]
-print(paths)
- # A:\common textbook 2\code\job testing\
+
 with open(paths[0],'r') as f:
     tests = json.loads(f.read())['tests']
 
@@ -24,13 +23,7 @@ def assign_values(tests):
 
 assign_values(tests)
 
-# print('======')
-# for test in tests:
-#     print(test)
-#     print('----')
-
 out = json.dumps({ 'tests': tests }, sort_keys=True, indent=4)
-
 
 with open('report.json', 'w') as f:
     f.write(out)
